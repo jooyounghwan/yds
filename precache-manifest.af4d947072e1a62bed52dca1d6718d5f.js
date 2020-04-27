49c1b0f4edd522e50dca59ccca4a22ce69a4c1da
Joo <wndudghks96@naver.com>
2020-04-27 23:57:05 +0900

    Updates

---
+++ precache-manifest.af4d947072e1a62bed52dca1d6718d5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf7501753e5647d93196579b177bf62",
+    "url": "/yds/index.html"
+  },
+  {
+    "revision": "1906de92e7b2ffe8f712",
+    "url": "/yds/static/js/2.3cfa99e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yds/static/js/2.3cfa99e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95c93b2fe1a2c1f69b2",
+    "url": "/yds/static/js/main.0854d0cc.chunk.js"
+  },
+  {
+    "revision": "91958160e8d9f6a2270e",
+    "url": "/yds/static/js/runtime-main.21d83d03.js"
+  },
+  {
+    "revision": "63115ec273aeb5778c9d1119eb4176fb",
+    "url": "/yds/static/media/IMDb.63115ec2.svg"
+  },
+  {
+    "revision": "6bc11c2ea87b959eb371dd2e91f4fb5b",
+    "url": "/yds/static/media/Loading.6bc11c2e.gif"
+  },
+  {
+    "revision": "13526e1eb815abe5be9b3acb01d9dccc",
+    "url": "/yds/static/media/NoVideo.13526e1e.png"
+  },
+  {
+    "revision": "d0ac7ce50ba7dfce6c9660500dafba20",
+    "url": "/yds/static/media/noPosterSmall.d0ac7ce5.png"
+  }
+]);
